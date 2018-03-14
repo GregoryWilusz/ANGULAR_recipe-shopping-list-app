ef7d4b45e33a01293f29ce75145b7805c54447e3
Greg <wilusz.gregory@gmail.com>
2018-03-14 10:48:37 +0100

    Added navigation with event binding and *ngIf

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: 'header.component.html'
 })
 export class HeaderComponent {
+  @Output() selectedTab = new EventEmitter<string>();
 
+  onSelect(choice: string) {
+    this.selectedTab.emit(choice);
+  }
 }
